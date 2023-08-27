e84711bfec58bf4cf808d7a1f4376e017403d34f
iq2v <142683864+iq2v@users.noreply.github.com>
2023-08-28 02:10:16 +0300

    day3
    day3

---
+++ day 2.py
@@ -0,0 +1,32 @@
+my_name=input("what is your name?")
+my_age=input("how old are you ?")
+print(f"my name is {my_name} and im {my_age} years old")
+first_number=int(input("first_number"))
+secound_number=int(input("secound_number"))
+operation=input("what is the operation(+-*/)")
+if operation == '+' :
+
+    print("first_number + second_number")
+
+elif operation== "-" :
+    print(first_number - secound_number)
+
+elif operation == '*' :
+    print ==("first_number * second_number")
+
+
+elif operation == "/" :
+    print("first_number / second_number")
+
+else : print("error")
+
+#part 3 
+bus_capacity = 20
+people_on_the_bus = int(input('how many people are on the bus ?'))
+empty_seats = bus_capacity - people_on_the_bus
+if empty_seats >= people_on_the_bus : 
+    print(f'there are {empty_seats} empty seats') 
+    elif empty_seats <= people_on_the_bus
+print('the bus is full')
+
+
---
+++ jana.py
@@ -0,0 +1,21 @@
+my_name=input("what is your name?")
+my_age=input("how old are you ?")
+print(f"my name is {my_name} and im {my_age} years old")
+first_number=int(input("first_number"))
+secound_number=int(input("secound_number"))
+operation=input("what is the operation(+-*/)")
+if operation == '+' :
+
+    print("first_number + second_number")
+
+elif operation== "-" :
+    print(first_number - secound_number)
+
+elif operation == '*' :
+    print ==("first_number * second_number")
+
+
+elif operation == "/" :
+    print("first_number / second_number")
+
+else :operation !=""
