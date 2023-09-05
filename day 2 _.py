f0b19e9de2328196a0eba5bb994afa3f61ed3279
iq2v <142683864+iq2v@users.noreply.github.com>
2023-09-05 17:03:44 +0300

    Update day 2 _.py

--- day 2 _.py
+++ day 2 _.py
@@ -6,27 +6,28 @@ secound_number=int(input("secound_number"))
 operation=input("what is the operation(+-*/)")
 if operation == '+' :
 
-    print("first_number + second_number")
+    print(first_number + secound_number)
 
 elif operation== "-" :
     print(first_number - secound_number)
 
 elif operation == '*' :
-    print("first_number * second_number")
+    print(first_number * secound_number)
 
 elif operation == "/" :
-    print("first_number / second_number")
+    print(first_number / secound_number)
 
 else : 
     print('error')
 
 #part 3 
 bus_capacity = 20
-people_on_the_bus = int(input('how many people are on the bus ?'))
+people_on_the_bus = int(input('How many people are on the bus ?'))
+want_to_ride = int(input("How many people want to take the bus ?"))
 empty_seats = bus_capacity - people_on_the_bus
 if empty_seats >= people_on_the_bus : 
     print(f'there are {empty_seats} empty seats') 
 else :
-    print('the bus is full')
+    print('The bus is full')
 
 
